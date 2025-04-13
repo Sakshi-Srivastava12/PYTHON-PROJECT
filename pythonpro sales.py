dc348b86af141eb396bc88df14abfcb8ee78e286
Sakshi Srivastava <149517312+Sakshi-Srivastava12@users.noreply.github.com>
2025-04-13 16:49:51 +0530

    Update pythonpro sales.py

--- pythonpro sales.py
+++ pythonpro sales.py
@@ -195,7 +195,7 @@ sns.heatmap(
 # Improve Labels
 plt.xticks(rotation=45, ha="right", fontsize=12)  # Rotate x-axis labels
 plt.yticks(fontsize=12)  # Increase y-axis font size
-plt.title("Sales Distribution Across Age Groups & Product Categories", fontsize=14, fontweight="bold")
+plt.title("Sales Distribution Across Age Groups & Product Categories", fontsize=15, fontweight="bold")
 plt.xlabel("Product Category", fontsize=12)
 plt.ylabel("Age Group", fontsize=12)
 
